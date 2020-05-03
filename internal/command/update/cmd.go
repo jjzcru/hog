6d69f2c1704da6feba02d3534962bd44d3c2461d
jjzcru <jjzcru@gmail.com>
2020-05-03 18:08:08 -0500

    Enable update file with regex

--- internal/command/update/cmd.go
+++ internal/command/update/cmd.go
@@ -14,7 +14,7 @@ func Command() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "update",
 		Short: "Update the files in a bucket by its id",
-		Args:  cobra.MinimumNArgs(2),
+		Args:  cobra.ExactArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 			id := args[0]
 			files := args[1:]
@@ -34,8 +34,9 @@ func run(id string, files []string) error {
 		return err
 	}
 
-	if _, ok := h.Buckets[id]; !ok {
-		return fmt.Errorf("bucket with id '%s' do not exist", id)
+	bucketID, err := getBucketID(h, id)
+	if err != nil {
+		return err
 	}
 
 	for i, file := range files {
@@ -45,13 +46,43 @@ func run(id string, files []string) error {
 		}
 
 		if !utils.IsPathExist(filePath) {
-			return fmt.Errorf("path %s is not valid or do not exist", filePath)
+			return fmt.Errorf("path '%s' is not valid or do not exist", filePath)
 		}
 
 		files[i] = filePath
 	}
 
-	h.Buckets[id] = files
+	h.Buckets[bucketID] = files
 
 	return hog.Save(h)
+}
+
+func getBucketID(h hog.Hog, id string) (string, error) {
+	var ids []string
+	response := ""
+
+	for k := range h.Buckets {
+		isSubstring, err := utils.IsSubstring(id, k)
+		if err != nil {
+			return response, err
+		}
+
+		if isSubstring {
+			ids = append(ids, k)
+		}
+	}
+
+	ids = utils.RemoveDuplicate(ids)
+
+	if len(ids) == 0 {
+		return response, fmt.Errorf("no bucket was found with id '%s'", id)
+	}
+
+	if len(ids) > 1 {
+		return response, fmt.Errorf("more than one bucket exists with the id '%s'", id)
+	}
+
+	response = ids[0]
+
+	return response, nil
 }
